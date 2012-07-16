b2df6b4eb29c8a798831d89f9cb30523bdbc4bb7
Angela Brooks <brooks@cga02.broadinstitute.org>
2012-07-16 16:54:35 -0400

    Updated createPseudoSample.py with --by_chr

--- createPseudoSample.py
+++ createPseudoSample.py
@@ -20,7 +20,7 @@ from helperFunctions import runCmd
 #############
 SHELL = "/bin/tcsh"
 
-NUM_GENOME_READS = 1000
+NUM_GENOME_READS = 10
 #################
 # END CONSTANTS #
 #################
@@ -87,6 +87,7 @@ def main():
 
     by_chr = options.by_chr
 
+    input_dir = None
     if os.path.exists(options.input_dir):
         input_dir = os.path.abspath(options.input_dir)
     else:
@@ -98,62 +99,128 @@ def main():
 
     samp = options.sample
 
+    # if by_chr is False, only one chr
+    chr2pseduo_dir = {}
+    
     # Create pseudo directory
-    pseudo_dir = options.input_dir + "/pseudo"
+    if by_chr:
+        # grab chr names from preProcess.bed files
+        chrs = getChr(input_dir)
+        if chrs == []:
+            print "Missing bed files from preProcess_getASEventReadCounts_by_chr_step2.py. They should be in: %s" % input_dir
+            opt_parser.print_help()
+            sys.exit(1)
 
-    if os.path.exists(pseudo_dir):
-        print "%s/pseudo already exists." % input_dir
-        opt_parser.print_help()
-        sys.exit(1)
+        for chr in chrs:
+            chr2pseudo_dir[chr] = input_dir + "/pseudo_" % chr
+    else:
+        pseudo_dir = input_dir + "/pseudo"
+        chr2pseudo_dir["all"] = pseudo_dir
 
-    os.mkdir(pseudo_dir)
+    for chr in chr2pseudo_dir:
+        if not os.path.exists(chr2pseudo_dir[chr]):
+            os.mkdir(chr2pseudo_dir[chr])
 
     # Make a link to the junction file made in
     # preProcess_getASEventReadCounts_step2.py
-    bed_file = input_dir + "/tmp_preProcess_getASEventReadCounts_step2.bed"
-    if not os.path.exists(bed_file):
-        print "Missing %s. Please run preProcess_getASEventReadCounts_step2.py" % bed_file
-        sys.exit(1)
+    if "all" in chr2pseudo_dir:
+        bed_file = input_dir + "/tmp_preProcess_getASEventReadCounts_step2.bed"
+        if not os.path.exists(bed_file):
+            print "Missing %s. Please run preProcess_getASEventReadCounts_step2.py" % bed_file
+            sys.exit(1)
 
-    ln_cmd = "ln -s %s %s/pseudo_junctions.bed" % (bed_file,
-                                                   pseudo_dir)
-    os.system(ln_cmd)
+        ln_cmd = "ln -s %s %s/pseudo_junctions.bed" % (bed_file,
+                                                       chr2pseudo_dir["all"])
+        os.system(ln_cmd)
+
+    else:
+        for chr in chr2pseudo_dir:
+            bed_file = input_dir + "/tmp_" + chr + "_preProcess_getASEventReadCounts_step2.bed"
+            ln_cmd = "ln -s %s %s/pseudo_%s/pseudo_%s_junctions.bed" % (bed_file,
+                                                                        input_dir,
+                                                                        chr,
+                                                                        chr)
+            os.system(ln_cmd)
 
     # Link to intron_exon_junction file
-    i_e_file = input_dir + "/%s/%s_intron_exon_junction_counts.txt" % (samp,
-                                                                       samp)
-    if not os.path.exists(i_e_file):
-        print "Missing file %s." % i_e_file
-        print "Please run preProcess_getASEventReadCounts_step3.py on sample %s." % samp
-        sys.exit(1)
+    if "all" in chr2pseudo_dir:
+        i_e_file = input_dir + "/%s/%s_intron_exon_junction_counts.txt" % (samp,
+                                                                           samp)
+        if not os.path.exists(i_e_file):
+            print "Missing file %s." % i_e_file
+            print "Please run preProcess_getASEventReadCounts_step3.py on sample %s." % samp
+            sys.exit(1)
 
-    ln_cmd = "ln -s %s %s/pseudo_intron_exon_junction_counts.txt" % (i_e_file,
-                                                                     pseudo_dir)
-    os.system(ln_cmd)
+        ln_cmd = "ln -s %s %s/pseudo_intron_exon_junction_counts.txt" % (i_e_file,
+                                                                         chr2pseudo_dir["all"])
+        os.system(ln_cmd)
+
+    else:
+        for chr in chr2pseudo_dir:
+            i_e_file = input_dir + "/%s/%s_%s/%s_%s_intron_exon_junction_counts.txt" % (samp,
+                                                                                        samp, chr,
+                                                                                        samp, chr)
+            if not os.path.exists(i_e_file):
+                print "Missing file %s." % i_e_file
+                print "Please run preProcess_getASEventReadCounts_step3.py on sample %s and chr %s." % (samp,
+                                                                                                        chr)
+                sys.exit(1)
+
+            ln_cmd = "ln -s %s %s/pseudo_%s_intron_exon_junction_counts.txt" % (i_e_file,
+                                                                                chr2pseudo_dir[chr],
+                                                                                chr)
+            os.system(ln_cmd)
+                    
 
     # Copy over a little of the genome reads
-    genome_file = input_dir + "/%s/%s_genome_reads.txt.gz" % (samp, samp)
-    pseudo_genome_file = pseudo_dir + "/pseudo_genome_reads.txt.gz"
-    
-    if not os.path.exists(genome_file):
-        print "Missing file %s" % genome_file
-        sys.exit(1)
+    if "all" in chr2pseudo_dir:
+        genome_file = input_dir + "/%s/%s_genome_reads.txt.gz" % (samp, samp)
+        pseudo_genome_file = pseudo_dir + "/pseudo_genome_reads.txt.gz"
+        
+        if not os.path.exists(genome_file):
+            print "Missing file %s" % genome_file
+            sys.exit(1)
 
-    genome_fh = gzip.open(genome_file, "rb")
-    pseudo_fh = gzip.open(pseudo_genome_file, "wb")
+        genome_fh = gzip.open(genome_file, "rb")
+        pseudo_fh = gzip.open(pseudo_genome_file, "wb")
 
-    line_ctr = 0
-    for line in genome_fh:
-        if NUM_GENOME_READS == line_ctr:
-            break
+        line_ctr = 0
+        for line in genome_fh:
+            if NUM_GENOME_READS == line_ctr:
+                break
 
-        pseudo_fh.write(line)
+            pseudo_fh.write(line)
 
-        line_ctr += 1
-    
-    genome_fh.close()
-    pseudo_fh.close()
-                                                                    
+            line_ctr += 1
+        
+        genome_fh.close()
+        pseudo_fh.close()
+
+    else:                                                                        
+        for chr in chr2pseudo_dir:
+            genome_file = input_dir + "%s/%s_%s/%s_%s_genome_reads.txt.gz" % (samp,
+                                                                              samp, chr,
+                                                                              samp, chr)
+            pseudo_file = pseudo_dir + "/pseudo_%s_genome_reads.txt.gz" % chr
+
+            if not os.path.exists(genome_file):
+                print "Missing file %s" % genome_file
+                sys.exit(1)
+
+            genome_fh = gzip.open(genome_file, "rb")
+            pseudo_fh = gzip.open(pseudo_genome_file, "wb")
+
+            line_ctr = 0
+            for line in genome_fh:
+                if NUM_GENOME_READS == line_ctr:
+                    break
+   
+                pseduo_fh.write(line)
+                line_ctr += 1
+
+            genome_fh.close()
+            pseudo_fh.close() 
+
     sys.exit(0)
 
 ############
@@ -168,6 +235,17 @@ def formatLine(line):
     line = line.replace("\n","")
     return line
 
+def getChr(input_dir):
+    chr_list = []
+    for subfile in input_dir:
+        if not subfile.endswith("preProcess_getASEventReadCounts_step2.bed"):
+            continue
+
+        first_split = bed_file.split("preProcess")[0]
+        second_split = first_split.split("tmp")[-1]
+        chr_list.append(second_split.strip("_"))
+
+    return chr_list
 #################
 # END FUNCTIONS #	
 #################	
